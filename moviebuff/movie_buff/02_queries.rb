3ddb3ba1fd6d3f54c49756b5dbf818e0e57c31d2
eartin <eugeneartin@gmail.com>
2019-05-30 14:20:59 -0700

    Add moviebuff to 01 spec

--- moviebuff/movie_buff/02_queries.rb
+++ moviebuff/movie_buff/02_queries.rb
@@ -2,12 +2,12 @@ def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between
   # 3 and 5 (inclusive).
   # Show the id, title, year, and score.
-
+  Movie.select(:id, :title, :yr, :score).where(score: 3..5).where(yr: 1980..1989)
 end
 
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
-
+  Movie.select(:yr).where('score <= 8').group('yr').pluck(:yr)
 end
 
 def cast_list(title)
